cae7886e76719018bd8fb53471cb6be33e54bd4b
ferdymercury <ferdymercury@users.noreply.github.com>
2022-03-23 08:26:37 +0100

    Read nthreads from env variable

--- documentation/doxygen/listlibs.sh
+++ documentation/doxygen/listlibs.sh
@@ -16,5 +16,4 @@ esac
 # Transform collaboration diagram into list of libraries
 
 #https://stackoverflow.com/questions/2425870/multithreading-in-bash
-NTHREADS=8
 grep -s "Collaboration diagram for"  $DOXYGEN_OUTPUT_DIRECTORY/html/class*.html | sed -e "s/.html:.*$//" | sed -e "s/^.*html\/class//" | xargs -P $NTHREADS -n 1 ./makelibs.sh
